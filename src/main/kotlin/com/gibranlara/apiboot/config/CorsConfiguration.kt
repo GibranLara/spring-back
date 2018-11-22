4ac5f57c6f73c0cd3fd872770f98e83b4f73f415
Unknown <gibranln@hotmail.com>
2018-11-21 22:35:09 -0600

    feat: CORS global para la API Rest.

---
+++ src/main/kotlin/com/gibranlara/apiboot/config/CorsConfiguration.kt
@@ -0,0 +1,20 @@
+package com.gibranlara.apiboot.config
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class CorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4873")
+            }
+        }
+    }
+}
