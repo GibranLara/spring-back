fa1e4ab55e117cbd94033d4937bf4d8d3f199d54
Unknown <gibranln@hotmail.com>
2019-02-16 17:25:23 -0600

    feat: version 1.0, genera .war y tiene frontend integrado

--- src/main/kotlin/com/gibranlara/apiboot/ApibootApplication.kt
+++ src/main/kotlin/com/gibranlara/apiboot/ApibootApplication.kt
@@ -1,12 +1,23 @@
 package com.gibranlara.apiboot
 
+import javafx.application.Application
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class ApibootApplication
+class ApibootApplication : SpringBootServletInitializer() {
 
-fun main(args: Array<String>) {
-    SpringApplication.run(ApibootApplication::class.java, *args)
+    protected override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+        return application.sources(Application::class.java)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(ApibootApplication::class.java, *args)
+        }
+    }
 }
